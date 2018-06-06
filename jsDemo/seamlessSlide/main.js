ad9fbd54a317adf46f4ddd486b625297cbca2be0
高远 <gyz@192.168.2.114>
2018-06-06 18:23:46 +0800

    new anli

---
+++ jsDemo/seamlessSlide/main.js
@@ -0,0 +1,75 @@
+//优化前
+
+$('.images > img:nth-child(1)').addClass('current')
+$('.images > img:nth-child(2)').addClass('enter')
+$('.images > img:nth-child(3)').addClass('enter')
+
+let n = 1
+setInterval(()=>{
+    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+        .one('transitionend',(e)=>{
+            $(e.currentTarget).removeClass('leave').addClass('enter');
+        })
+    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+    n += 1
+},3000)
+
+function x(n){
+    if(n>3){
+        n = n%3
+        if(n === 0){
+            n = 3
+        }
+    }
+    return n
+}
+
+
+//优化后
+
+// let n 
+// w()
+// setInterval(()=>{
+//     makeLeave(getImage(n))
+//     .one('transtionend',(e)=>{
+//         makeEnter($(e.currentTarget))
+//     })
+//     makeCurrent(getImage(n+1))
+//     n += 1
+// },3000)
+
+
+
+
+// function getImage(n){
+//     return $(`.images > img:nth-child(${x(n)})`)
+// }
+
+// function x(n){
+//     if(n>3){
+//         n = n%3
+//         if(n === 0){
+//             n = 3
+//         }
+//     }
+//     return n
+// }
+
+// function w(){
+//     n = 1
+//     $(`.images > img:nth-child(${n})`).addClass('current')
+//     .siblings().addClass('enter')
+// }
+
+// function makeCurrent($node){
+//     return $node.removeClass('enter leave').addClass('current')
+// }
+// function makeLeave($node){
+//     return $node.removeClass('enter current').addClass('leave')
+// }
+// function makeEnter($node){
+//     return $node.removeClass('leave current').addClass('enter')
+// }
+
+
+
