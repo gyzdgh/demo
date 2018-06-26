f798970f90971b9c5cf06e02ae8983956d7a28c9
高远 <gyz@192.168.2.114>
2018-06-26 20:36:59 +0800

    用setTimerout做的导航，有bug

---
+++ jsDemo/setTimeout-nav/main.js
@@ -0,0 +1,44 @@
+var get = {
+    byId : function(id){
+        return document.getElementById(id)
+    },
+    byClass : function(sClass,oParent){
+        var aClass = [];
+        var reClass = new RegExp("(^| )" + sClass + "( |$)");
+        var aElem = this.byTagName("*",oParent);
+        for(var i=0;i<aElem.length;i++) reClass.test(aElem[i].className) && aClass.push(aElem[i]);
+        return aClass
+    },
+    byTagName : function(elem,obj){
+        return (obj || document).getElementsByTagName(elem)
+    }
+};
+window.onload = function(){
+    var oNav = get.byId("nav");
+    var aLi = get.byTagName("li",oNav);
+    var aSubNav = get.byClass("subnav",oNav);
+    var oSubNav = oEm = timer = null;
+    var i=0;
+    for(i=1;i<aLi.length;i++){
+        aLi[i].onmouseover = function(){
+            for(var i=0;i<aSubNav.length;i++)aSubNav[i].style.display = "none";
+            oSubNav = get.byClass("subnav",this)[0];
+            oEm = get.byTagName("em",this)[0];
+            oSubNav.style.display = "block";
+            oNav.offsetWidth - this.offsetLeft > oSubNav.offsetWidth ?
+            oSubNav.style.left = this.offsetLeft + "px" :
+            oSubNav.style.right = 0;
+            oEm.style.left = this.offsetLeft - oSubNav.offsetLeft + 50 + "px";
+            clearTimeout(timer);
+            oSubNav.onmouseover = function(event){
+                (event || window.event).cancelBubble = true;
+                clearTimeout(timer);
+            }
+        };
+        aLi[i].onmouseout = function(){
+            timer = setInterval(function(){
+                oSubNav.style.display = "none"
+            },300)
+        }       
+    }
+};
